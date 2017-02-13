c9561b93354ac26ef8a3c63b7968cf3ca4ef12be
LuizH <luizh_nunes@hotmail.com>
2017-02-13 21:21:07 -0200

    USANDO CONST

--- app/Http/Controllers/CursosController.php
+++ app/Http/Controllers/CursosController.php
@@ -6,18 +6,18 @@ use Illuminate\Http\Request;
 
 class CursosController extends Controller
 {
-    public $cursos = array(
+    const CURSOS = array(
       'Lógica de programação', 'Desenvolvimento Web com PHP',
       'Algoritmos e estrutura de dados', 'Test Driven Development'
     );
 
     public function index() {
-      $cursos = $this->cursos;
+      $cursos = SELF::CURSOS;
       return view('cursos.index', compact('cursos'));
     }
 
     public function show($index) {
-      $curso = $this->cursos[$index];
+      $curso = SELF::CURSOS[$index];
       return view('cursos.show', compact('curso'));
     }
 }
